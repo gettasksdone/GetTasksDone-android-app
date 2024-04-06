e22c484e90af92a8b262633283f6683f610744d3
jose.lopez <jose.lopez@valid.com>
2024-04-06 19:06:29 +0200

    completar registro

---
+++ app/src/main/java/com/example/myapplication/data/JwtHelper.kt
@@ -0,0 +1,30 @@
+package com.example.myapplication.data
+
+import android.content.Context
+import com.auth0.jwt.JWT
+import com.auth0.jwt.interfaces.DecodedJWT
+import com.example.myapplication.util.PreferenceHelper
+import com.example.myapplication.util.PreferenceHelper.get
+
+class JwtHelper(private val context: Context){
+    private fun getJwt(): String {
+        val preferences = PreferenceHelper.defaultPrefs(context)
+        return preferences["jwt"]
+    }
+
+    private fun decodeJWT(token: String): DecodedJWT {
+        return JWT.decode(token)
+    }
+
+    public fun getId(): Int {
+        val token = getJwt()
+        val decodedToken = decodeJWT(token)
+        val idClaim = decodedToken.getClaim("id")
+        val idString = idClaim.asString()
+        return idString.toInt()
+    }
+
+    public fun getToken(): String {
+        return getJwt()
+    }
+}
--- app/src/main/java/com/example/myapplication/io/ApiService.kt
+++ app/src/main/java/com/example/myapplication/io/ApiService.kt
@@ -6,11 +6,15 @@ import com.google.gson.GsonBuilder
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.Body
+import retrofit2.http.Header
 import retrofit2.http.POST
 
 data class LoginRequest(val username: String, val password: String)
 data class RegisterRequest(val username: String, val password: String, val email: String)
+data class UsuarioId(val id: Int)
+data class CompleteRegisterRequest(val usuario: UsuarioId, val nombre: String, val telefono: String, val puesto: String, val departamento: String)
 fun provideGson(): Gson {
     val builder = GsonBuilder()
     builder.setLenient()
@@ -23,11 +27,18 @@ interface ApiService {
     @POST(value = "/auth/register")
     fun postRegister(@Body request: RegisterRequest): Call<String>
 
+    @POST(value = "/userData/create")
+    fun completeRegister(
+        @Header("Authorization") authHeader: String?,
+        @Body request: CompleteRegisterRequest
+    ): Call<String>
+
     companion object Factory{
         private const val BASE_URL = "https://lopezgeraghty.com:8080"
         fun create(): ApiService{
             val retrofit = Retrofit.Builder()
                 .baseUrl(BASE_URL)
+                .addConverterFactory(ScalarsConverterFactory.create())
                 .addConverterFactory(GsonConverterFactory.create(provideGson()))
                 .build()
             return retrofit.create((ApiService::class.java))
