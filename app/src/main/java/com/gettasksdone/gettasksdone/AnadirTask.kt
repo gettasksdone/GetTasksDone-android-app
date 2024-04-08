b6fd99a78090978e2c23311713c1e239f029c471
Camilo Andres D'isidoro <cdisidoro99@gmail.com>
2024-04-08 16:05:02 +0200

    Cambios de Victor

--- app/src/main/java/com/gettasksdone/gettasksdone/AnadirTask.kt
+++ app/src/main/java/com/gettasksdone/gettasksdone/AnadirTask.kt
@@ -3,7 +3,10 @@ package com.gettasksdone.gettasksdone
 import android.graphics.Color
 import android.os.Bundle
 import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.ImageButton
+import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -19,6 +22,26 @@ class AnadirTask : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        //Spinner
+        val contextos = mutableListOf("contexto", "Contexto 1", "Contexto 2", "añadir nuevo contexto") //contextos de ejemplo luego seran los de la API
+        val spinner: Spinner = findViewById(R.id.spinner)
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, contextos)
+        spinner.adapter = adapter
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+                val seleccionado = parent.getItemAtPosition(position).toString()
+                if (seleccionado == "añadir nuevo contexto") {
+                    // Aquí puedes manejar la acción de añadir un nuevo contexto
+                } else {
+                    // Aquí puedes manejar la selección de un contexto
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
         }
     }
     fun onStarClick(view: View) { //Descomentar cuando este hecho lo del booleano
