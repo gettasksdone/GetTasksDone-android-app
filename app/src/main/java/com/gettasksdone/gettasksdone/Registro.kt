30d7707c61ecff20e4f68e20a79152dc6f83966b
jcandela@ucm.es <jcandela@ucm.es>
2024-04-27 19:47:38 +0200

    sesion Registro con opción de servidor 27-04-2024

--- app/src/main/java/com/gettasksdone/gettasksdone/Registro.kt
+++ app/src/main/java/com/gettasksdone/gettasksdone/Registro.kt
@@ -4,24 +4,36 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Spinner
 import android.widget.TextView
 import android.widget.Toast
 import com.gettasksdone.gettasksdone.io.ApiService
 import com.gettasksdone.gettasksdone.io.requests.RegisterRequest
+import com.gettasksdone.gettasksdone.ui.Utils.AgregarUrlDialogFragment
 import com.gettasksdone.gettasksdone.util.PreferenceHelper
 import com.gettasksdone.gettasksdone.util.PreferenceHelper.get
 import com.gettasksdone.gettasksdone.util.PreferenceHelper.set
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import kotlin.properties.Delegates
 
-class Registro : AppCompatActivity() {
+class Registro : AppCompatActivity() , AgregarUrlDialogFragment.NewUrlDialogListener {
 
     private val apiService: ApiService by lazy{
         ApiService.create()
     }
+
+    private lateinit var serverSpinner: Spinner
+    private lateinit var serverAdapter: ArrayAdapter<String>
+    private var selectedUrl by Delegates.notNull<String>()
+    private var urlList: MutableSet<String> = mutableSetOf()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registro)
@@ -40,6 +52,34 @@ class Registro : AppCompatActivity() {
         btnGoLogin.setOnClickListener {
             goToLogin()
         }
+
+        //
+
+        serverSpinner = findViewById(R.id.selector_servidor)
+        serverAdapter = ArrayAdapter(this, R.layout.spinner_list, mutableListOf())
+        serverAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        serverSpinner.adapter = serverAdapter
+        serverSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>,
+                view: View,
+                position: Int,
+                id: Long
+            ) {
+                val selectedItem = parent.getItemAtPosition(position).toString()
+                if(selectedItem == "Añadir un nuevo servidor"){
+                    openNewUrlActivity()
+                }else{
+                    selectedUrl = urlList.elementAt(position)
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                Toast.makeText(applicationContext, "Se debe seleccionar un servidor", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        loadUrls()
     }
 
     private fun goToLogin(){
@@ -64,6 +104,41 @@ class Registro : AppCompatActivity() {
         val etNombre = findViewById<EditText>(R.id.etUsername).text.toString()
         val etEmail = findViewById<EditText>(R.id.etCorreo).text.toString()
         val etPassword = findViewById<EditText>(R.id.etPassword).text.toString()
+        val selectSpinner = findViewById<Spinner>(R.id.selector_servidor)
+        val textSpinner = selectSpinner.selectedItem.toString()
+        Toast.makeText(applicationContext, "Debug server $textSpinner", Toast.LENGTH_SHORT).show()
+        Log.w("Debug server", textSpinner)
+
+
+        if (textSpinner == ""){
+            Toast.makeText(applicationContext, "El campo Servidor es obligatorio", Toast.LENGTH_SHORT).show()
+            return;
+        }
+
+        if(etNombre == ""){
+            Toast.makeText(applicationContext, "El campo nombre de usuario es obligatorio", Toast.LENGTH_SHORT).show()
+            return;
+        }
+        if(etPassword == ""){
+            Toast.makeText(applicationContext, "El campo contraseña es obligatorio", Toast.LENGTH_SHORT).show()
+            return;
+        }
+
+        if(etEmail == ""){
+            Toast.makeText(applicationContext, "El campo Email es obligatorio", Toast.LENGTH_SHORT).show()
+            return;
+        }
+
+
+        try {
+            ApiService.Factory.setBaseUrl(textSpinner)
+        } catch (e: IllegalArgumentException) {
+            throw  IllegalArgumentException("El formato de la URL no es válido")
+        } catch (e: IllegalStateException) {
+            throw  IllegalStateException("No se ha establecido la URL")
+        }finally {
+            Toast.makeText(applicationContext, "DEBUG: url_base $textSpinner", Toast.LENGTH_SHORT).show()
+        }
 
         val registerRequest = RegisterRequest(username = etNombre, email = etEmail, password = etPassword)
         //Log.d("username:", "$etEmail")
@@ -104,4 +179,77 @@ class Registro : AppCompatActivity() {
         })
 
     }
+
+
+    override fun onDialogPositiveClick(newUrl: String) {
+
+        if(newUrl.isEmpty()) {
+            Toast.makeText(
+                applicationContext,
+                "La URL del servidor no puede estar vacía",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+        else if (!newUrl.startsWith("http://") &&!newUrl.startsWith("https://")) {
+            Toast.makeText(
+                applicationContext,
+                "La URL del servidor debe empezar con http:// o https://",
+                Toast.LENGTH_SHORT
+            ).show()
+
+            /*
+             else if(!isValidDomain(newUrl) && !isValidDomain(newUrl)){
+
+           Toast.makeText(applicationContext, "La URL del servidor no es válida", Toast.LENGTH_SHORT).show()
+        }
+
+             */
+
+        }else {
+
+            val preferencesTest = PreferenceHelper.defaultPrefs(applicationContext)
+            val urlList = preferencesTest.getStringSet("urlList", emptySet())
+            val newUrlList = mutableSetOf<String>()
+            if (urlList != null) {
+                newUrlList.addAll(urlList)
+                with(preferencesTest.edit()) {
+                    newUrlList.add(newUrl)
+                    putStringSet("urlList", newUrlList)
+                    apply()
+                }
+                Toast.makeText(
+                    applicationContext,
+                    "La URL del servidor ha sido agregada correctamente",
+                    Toast.LENGTH_SHORT
+                ).show()
+                loadUrls()
+            }
+        }
+    }
+
+    private fun openNewUrlDialog() {
+        val dialog = AgregarUrlDialogFragment()
+        dialog.show(supportFragmentManager, "AgregarUrlDialog")
+    }
+
+    private fun openNewUrlActivity() {
+        openNewUrlDialog()
+    }
+
+    private fun loadUrls() {
+        val preferencesTest = PreferenceHelper.defaultPrefs(applicationContext)
+        val urlList = preferencesTest.getStringSet("urlList", emptySet())
+        if(urlList != null){
+            if(urlList.isNotEmpty()){
+                Log.w("URLs", urlList.toString())
+                this.urlList.clear()
+                this.urlList = urlList
+                this.urlList.add("Añadir un nuevo servidor")
+                serverAdapter.clear()
+                serverAdapter.addAll(this.urlList)
+            }
+        }
+    }
+
+
 }
